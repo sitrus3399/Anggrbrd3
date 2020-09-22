dfa94d0d624be7f469fcb924b63bb6aee3fb9803
rehunter <hunter.kilua99@gmail.com>
2020-09-22 13:04:28 +0700

    no message

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Script/OwlBird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public GameObject bird;
+    private Vector3 scaleExlopde;
+    private bool isExplode = false;
+
+    public override void Explode()
+    {
+        if (!isExplode)
+        {
+            scaleExlopde = new Vector3(1f, 1f, 1f);
+            bird.transform.localScale += scaleExlopde;
+            bird.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            StartCoroutine(DestroyAfter(.1f));
+            isExplode = true;
+        }
+    }
+
+    private IEnumerator DestroyAfter(float second)
+    {
+        yield return new WaitForSeconds(second);
+        Destroy(gameObject);
+    }
+}
